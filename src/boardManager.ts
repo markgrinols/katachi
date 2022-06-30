fdd13616c59e0029d1f2380be5756b4eccf7b8e6
Mark Grinols <maakugrinols@gmail.com>
2022-06-30 15:45:00 -0700

    puzzle loader and logic fixes

--- src/boardManager.ts
+++ src/boardManager.ts
@@ -3,10 +3,30 @@ import store from './store';
 import {clearUserClicked} from './reducers/input';
 import {loadBoardDataNow} from './reducers/app';
 import {updateCells, setRowsCols} from './reducers/board';
+import {PuzzleLoader} from './PuzzleLoader';
 
 export class BoardManager {
+  puzzle!: {region: number[], dimensions: number[], circles: number,
+      otherShapes: number[], id: number, vals: number[], givens: number[]};
+
   constructor() {
     store.subscribe(() => this.onStateUpdated());
+  }
+
+  loadBoardData() {
+    store.dispatch(loadBoardDataNow(false));
+    const loader = new PuzzleLoader();
+    this.puzzle = loader.getPuzzle();
+    const rows = this.puzzle.dimensions[0];
+    const cols = this.puzzle.dimensions[1];
+
+    store.dispatch(setRowsCols([rows, cols]));
+
+    const cellUpdates = this.puzzle.vals.map((v, i) => {
+      const val = (this.puzzle.givens.includes(i)) ? v : 0;
+      return [i, val];
+    });
+    store.dispatch(updateCells({cellUpdates}));
   }
 
   onStateUpdated() {
@@ -20,33 +40,21 @@ export class BoardManager {
       store.dispatch(clearUserClicked(null));
 
       const index = state.board.cols * row + col;
-      const currVal = state.board.cells[index];
-      const payload = [index, (currVal + 1)% 5];
-      const cellUpdates = [payload];
-      store.dispatch(updateCells({cellUpdates}));
+      if (!this.puzzle.givens.includes(index)) {
+        const currVal = state.board.cells[index];
+        const newVal = (currVal + 1) % 5;
+        const payload = [index, newVal];
+        if (this.puzzle.vals[index] === newVal) {
+          console.log('nice');
+        }
+        const cellUpdates = [payload];
+        store.dispatch(updateCells({cellUpdates}));
+      }
     }
 
     const appState = state.app;
     if (appState.loadBoardDataNow) {
-      store.dispatch(loadBoardDataNow(false));
-      console.log(`load data now: ${appState.loadBoardDataNow}`);
-
-      // square3x3,25006,2013-07-12T13:47:59.405Z,1.2173912525177002,25006,true,"<p id=""25006"" lb=""2"" ab=""3,4"">
-      // answers: 211322333223332131333311222133233122133223312222113333311222333332131223322333211
-      // givens: 34,52,27,4,50,43,59,75,12,38,47,2,14,76,30,46,16,6,21,8,13,61,66,28,42,22,54,17,32,77,71,64,45,0,40,53,69,29,37,49,26,19,36,20,5,24
-
-      const boardData = [0, 1, 2, 3, 0, 3, 2, 0, 1, 0, 1, 2, 3, 0, 3, 2, 0, 1, 0, 1, 2, 3, 0, 3, 2, 0, 1,
-        0, 1, 2, 3, 0, 3, 2, 0, 1, 0, 1, 2, 3, 0, 3, 2, 0, 1, 0, 1, 2, 3, 0, 3, 2, 0, 1,
-        0, 1, 2, 3, 0, 3, 2, 0, 1, 0, 1, 2, 3, 0, 3, 2, 0, 1, 0, 1, 2, 3, 0, 3, 2, 0, 1,
-      ];
-      const rows = 9;
-      const cols = 9;
-
-      store.dispatch(setRowsCols([rows, cols]));
-      const cellUpdates = boardData.map((v, i) => {
-        return [i, v];
-      });
-      store.dispatch(updateCells({cellUpdates}));
+      this.loadBoardData();
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -26,4 +26,8 @@ todo:
  - load board data from file on remote endpoint
  - when cell changes, evaluate good/bad
  - build out board in renderer after cols/rows has been set
+ - hollow shapes
+ - count determines number of shapes so - 2 is 'plus', 3 is triangle, 4 is square, 5 is star
+ - stick with pastel colors for sure
+
 */
